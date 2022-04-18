912afdf9312ca3a2a3b3dca873fd68d0ba4ba46f
noriokaka <aki.na@live.jp>
2022-04-18 14:01:55 +0900

    得点管理アプリ作成2

--- NagatsuMain.java
+++ NagatsuMain.java
@@ -5,14 +5,12 @@ public class NagatsuMain{
 		System.out.print("生徒は何人>");
 		int num=scan.nextInt();
 		Student[] students = new Student[num];
-		int[] scores = new int[num];
 		for(int i=0;i<num;i++){
 			int id=i+1;
 			System.out.printf("%d人目の名前>",id);
 			String name=scan.next();
 			System.out.printf("%d人目の点数>",id);
 			int score=scan.nextInt();
-			scores[i]=score;
 			students[i]=new Student(id,name,score);
 		}
 		System.out.print("操作を入力:1...登録順に一覧,2...点数降順に一覧,3...終了>");
@@ -23,12 +21,12 @@ public class NagatsuMain{
 			}
 		}
 		if(command==2){
-			for(int i=0;i<scores.length-1;i++){
-				for(int j=i+1;j<scores.length;j++){
-					if(scores[i]<scores[j]){
-						int temp1=scores[i];
-						scores[i]=scores[j];
-						scores[j]=temp1;
+			for(int i=0;i<students.length-1;i++){
+				for(int j=i+1;j<students.length;j++){
+					if(students[i].score<students[j].score){
+						int temp1=students[i].score;
+						students[i].score=students[j].score;
+						students[j].score=temp1;
 						Student temp2=students[i];
 						students[i]=students[j];
 						students[j]=temp2;
